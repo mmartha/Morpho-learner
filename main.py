ecd8332c46c21238b085de579b7cc29d4bf65d45
mmartha <marthaghinrichs@gmail.com>
2015-03-05 15:51:02 -0500

    removed old tests from main

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
         # Analyze morphology from a data set
         lang = raw_input("\nEnter Turkish, Swahili, Zulu, or English\n")
         if lang in solutions.keys:
+            # If soltion set exists, attempt to find best parameters A and B
             A, B = morpho.findAB(data[lang], solutions[lang])
             affixes = morpho.getAffixDict(data[lang], A, B)
         else:
@@ -24,34 +25,6 @@ def main():
         # Print all morphemes from top scored to lowest
         morpho.printByMax(affixes)
     else: print "Choice not recognized, try again"
-
-
-    # To analyze the morphology from a Wikipedia page
-    '''USE_THIS_URL = url2
-    content = wiki.getContent(USE_THIS_URL)
-    text = wiki.cleanContent(content)
-    words, wordcount = wiki.getDictionary(text)'''
-   
-'''
-    # Analyze morphology from a data set
-    A, B = morpho.findAB(data["Turkish"], solutions["Turkish"])
-    print 
-    print "A = ", A, "B = ", B
-    affixes = morpho.getAffixDict(data["Turkish"], A, B)
-
-    # Print all morphemes from top scored to lowest
-    morpho.printByMax(affixes)
-'''
-
-    # Print top ranked possible segmentations
-'''for word in Zulu:
-        segments = morpho.possibleSegments(word)
-        ratings = {tuple(s):morpho.scoreSegmentation(list(s), affixes) for s in segments}
-        print word
-        morpho.printByMax(ratings, 5)
-        print "-------------------------------"
-        print
-'''
 
 
 if __name__ == '__main__':
