f08c0fd4bb5b9c355e9d851277ae0e7a9a6382f2
mmartha <marthaghinrichs@gmail.com>
2015-03-20 00:20:19 -0400

    fixed terminal message in main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dataset import data, solutions
 # Wikipedia articles in multiple languages
 links = {
     "English" : "http://en.wikipedia.org/wiki/World_War_II",
-    "Turkish" : "http://tr.wikipedia.org/wiki/II._D%C3%BCnya_Sava%C5%9F%C4%B1",
+    "Turkish" : "http://tr.wikipedia.org/wiki/Y%C3%BCz_(organ)",
     "French" : "http://fr.wikipedia.org/wiki/Seconde_Guerre_mondiale",
     "German" : "http://de.wikipedia.org/wiki/Zweiter_Weltkrieg",
     "Boarisch" : "http://bar.wikipedia.org/wiki/Wean"
@@ -20,14 +20,14 @@ def main():
         lang = raw_input("\nEnter Turkish, Swahili, Zulu, or English\n")
         dataset = data[lang]
 
-    if int(choice) ==2:
+    elif int(choice) ==2:
         lang = raw_input("\nEnter English, Turkish, French, or German\n")
         dataset, wordcount = wiki.get_dataset(links[lang])
     else: print "Choice not recognized, try again"
 
     mygraph = morpho.createGraph(dataset)
     for word in dataset:
-        print word, morpho.scoreSegmentation(word, mygraph)
+        print word, morpho.scoreSegmentation(word, mygraph)[0]
     
 
 
